212bd4b2a5b440bd675e7e8d46495c7c5cb0dcfd
RishabhSpark <rishabh1000khandelwal@gmail.com>
2022-11-27 18:07:38 +0530

    added

---
+++ mysql/196. Delete Duplicate Emails.sql
@@ -0,0 +1,7 @@
+# Please write a DELETE statement and DO NOT write a SELECT statement.
+# Write your MySQL query statement below
+DELETE p2
+FROM Person p1
+INNER JOIN Person p2
+ON p1.Email = p2.Email
+WHERE p1.id<p2.id

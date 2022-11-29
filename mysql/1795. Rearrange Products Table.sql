9ba7c6cf9d28ad702c3c645c57974b802c69fc9e
RishabhSpark <rishabh1000khandelwal@gmail.com>
2022-11-30 00:11:01 +0530

    added

---
+++ mysql/1795. Rearrange Products Table.sql
@@ -0,0 +1,16 @@
+# Write your MySQL query statement 
+SELECT product_id, 'store1' as store, store1 as price
+FROM products
+WHERE store1 IS NOT NULL
+
+UNION
+
+SELECT product_id, 'store2' as store, store2 as price
+FROM products
+WHERE store2 IS NOT NULL
+
+UNION
+
+SELECT product_id, 'store3' as store, store3 as price
+FROM products
+WHERE store3 IS NOT NULL

469e7f3daf8efc6dcde8fd1a0fcd2ac075260ea1
RishabhSpark <rishabh1000khandelwal@gmail.com>
2022-12-04 17:05:40 +0530

    added

---
+++ mysql/1393. Capital Gain Loss.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT stock_name,
+SUM(CASE WHEN operation='Buy' THEN price*-1
+    WHEN operation='Sell' THEN price
+    END) as capital_gain_loss
+FROM Stocks
+GROUP BY stock_name

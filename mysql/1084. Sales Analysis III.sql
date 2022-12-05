8fee811a63c66582d21f35226ad0c01bac7e8f44
RishabhSpark <rishabh1000khandelwal@gmail.com>
2022-12-05 22:40:49 +0530

    added

--- mysql/1084. Sales Analysis III.sql
+++ mysql/1084. Sales Analysis III.sql
@@ -1,6 +1,13 @@
 # Write your MySQL query statement below
-SELECT p.product_id, p.product_name
-FROM Product as p
-WHERE product_id NOT IN (SELECT p.product_id
-FROM Product as p JOIN Sales as s ON p.product_id = s.product_id
-WHERE s.sale_date<'2019-01-01' OR s.sale_date>'2019-03-31')
+# SELECT p.product_id, p.product_name
+# FROM Product as  p
+# WHERE product_id NOT IN (SELECT p.product_id
+# FROM Product as p JOIN Sales as s ON p.product_id = s.product_id
+# WHERE s.sale_date<'2019-01-01' OR s.sale_date>'2019-03-31')
+SELECT product_id, product_name
+FROM product
+JOIN SALES
+USING(product_id)
+GROUP BY product_id
+HAVING sum(CASE WHEN sale_date between '2019-01-01' and '2019-03-31' THEN 1 ELSE 0 end) > 0
+AND sum(CASE WHEN sale_date between '2019-01-01' and '2019-03-31' THEN 0 else 1 end) = 0

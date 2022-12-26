d4480da4a469b22167be98c9e5f6b4024fc9dfaf
RishabhSpark <rishabh1000khandelwal@gmail.com>
2022-12-26 16:48:57 +0530

    added

--- geeksforgeeks/Prime Number.cpp
+++ geeksforgeeks/Prime Number.cpp
@@ -1,15 +1,26 @@
-class Solution{
-public:
-    int isPrime(int N){
+int isPrime(int N){
         // code here
-        int is_prime = 1;
         if(N==1) return 0;
-        for(int i=2; i<=N/i; i++){
-            if(N%i==0) is_prime=0;
+        if(N==2 || N==3) return 1;
+        if(N%2==0 || N%3==0) return 0;
+        for(int i=5; i*i=n; i+=6){
+            if(N%i==0|| N%(i+2)==0) return 0;
         }
-        return is_prime;
+        return 1;
     }
-};
+
+// class Solution{
+// public:
+//     int isPrime(int N){
+//         // code here
+//         int is_prime = 1;
+//         if(N==1) return 0;
+//         for(int i=2; i<=N/i; i++){
+//             if(N%i==0) is_prime=0;
+//         }
+//         return is_prime;
+//     }
+// };
 
 
 // class Solution{

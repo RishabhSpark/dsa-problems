2d44b050a7d985598267bb22002fe9b562393dcd
RishabhSpark <rishabh1000khandelwal@gmail.com>
2022-12-26 22:24:40 +0530

    added

---
+++ Java Classes Introduction.java
@@ -0,0 +1,26 @@
+class task
+{
+    int length;
+    int width;
+    int height;
+    public void set_length(int l)
+    {
+        //Add your code here.
+        this.length = l;
+    }
+    public void set_width(int w)
+    {
+        //Add your code here.
+        this.width = w;
+    }
+    public void set_height(int h)
+    {
+        //Add your code here.
+        this.height = h;
+    }
+    public void volume()
+    {
+        //Add your code here.
+        System.out.println(length*width*height);
+    }
+}

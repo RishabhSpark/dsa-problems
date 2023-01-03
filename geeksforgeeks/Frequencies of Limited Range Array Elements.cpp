5286e49e419d9b06428351a69798946076021e38
RishabhSpark <rishabh1000khandelwal@gmail.com>
2023-01-03 20:23:28 +0530

    added

---
+++ geeksforgeeks/Frequencies of Limited Range Array Elements.cpp
@@ -0,0 +1,15 @@
+class Solution{
+    public:
+    //Function to count the frequency of all elements from 1 to N in the array.
+    void frequencyCount(vector<int>& arr,int N, int P)
+    { 
+        // code here
+        unordered_map<int, int>mp;
+        for(int i=0; i<N; i++){
+            mp[arr[i]]++;
+        }
+        for(int i=0; i<N; i++){
+            arr[i]=mp[i+1];
+        }
+    }
+};

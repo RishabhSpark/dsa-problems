bc5c44165e5b1c31aa86b87f5a7c013c0f8bd199
RishabhSpark <rishabh1000khandelwal@gmail.com>
2023-01-03 21:16:46 +0530

    added

--- leetcode/Algorithms/217. Contains Duplicate.cpp
+++ leetcode/Algorithms/217. Contains Duplicate.cpp
@@ -1,10 +1,25 @@
+// USING UNORDERED SET
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        for(int i=1; i<nums.size(); i++){
-            if(nums[i]==nums[i-1]) return true;
+        unordered_set<int> s;
+        for(int i=0; i<nums.size(); i++){
+            if(s.find(nums[i])!=s.end()) return true;
+            s.insert(nums[i]);
         }
         return false;
     }
 };
+
+
+// USING ARRAY(SORTING)
+// class Solution {
+// public:
+//     bool containsDuplicate(vector<int>& nums) {
+//         sort(nums.begin(), nums.end());
+//         for(int i=1; i<nums.size(); i++){
+//             if(nums[i]==nums[i-1]) return true;
+//         }
+//         return false;
+//     }
+// };

5ab3ba88e85f9e010021056c68c99d08ff1b7831
RishabhSpark <rishabh1000khandelwal@gmail.com>
2023-01-03 20:05:47 +0530

    added

---
+++ geeksforgeeks/Wave Array.cpp
@@ -0,0 +1,20 @@
+class Solution{
+    public:
+    // arr: input array
+    // n: size of array
+    //Function to sort the array into a wave-like array.
+    void convertToWave(int n, vector<int>& arr){
+        
+        // Your code here
+        if(n%2==0){
+            for(int i=0; i<n; i+=2){
+                swap(arr[i],arr[i+1]);
+            }
+        }
+        else{
+            for(int i=0; i<n-1; i+=2){
+                swap(arr[i],arr[i+1]);
+            }
+        }
+    }
+};

c1a7b767e60ee0b3b86f57ce4569506f316c5cfb
RishabhSpark <rishabh1000khandelwal@gmail.com>
2023-01-03 21:49:07 +0530

    added

--- leetcode/Algorithms/242. Valid Anagram.cpp
+++ leetcode/Algorithms/242. Valid Anagram.cpp
@@ -1,11 +1,32 @@
+// USING HASHING O(n)
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        if(t==s){
-            return true;
+        unordered_map<char, int> mp1;
+        unordered_map<char, int> mp2;
+        if(s.length()!=t.length()) return false;
+        for(int i=0; i<s.length(); i++){
+            mp1[s[i]]++;
+            mp2[t[i]]++;
         }
-        return false;
+        for (int i = 0; i < s.size(); i++){
+            if (mp1[s[i]] != mp2[s[i]]){
+                return false;
+            }
+        }
+        return true;
     }
 };
+
+// USING SORTING O(nlogn)
+// class Solution {
+// public:
+//     bool isAnagram(string s, string t) {
+//         sort(s.begin(), s.end());
+//         sort(t.begin(), t.end());
+//         if(t==s){
+//             return true;
+//         }
+//         return false;
+//     }
+// };

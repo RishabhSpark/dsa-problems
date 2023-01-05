5fb9054b679d4ab55f90935008b298297a7abe22
RishabhSpark <rishabh1000khandelwal@gmail.com>
2023-01-05 20:29:00 +0530

    added

--- leetcode/Algorithms/53. Maximum Subarray.cpp
+++ leetcode/Algorithms/53. Maximum Subarray.cpp
@@ -1,12 +1,26 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int ans=nums[0],i,sum=0;
-        for(i=0;i<nums.size();i++){
-            sum+=nums[i];
-            ans=max(sum,ans);
-            sum=max(sum,0);
+        int ans=nums[0];
+        int k=nums[0];
+        for(int i=1; i<nums.size(); i++){
+            k=max(k+nums[i], nums[i]);
+            ans=max(ans, k);
         }
         return ans;
     }
 };
+
+
+// class Solution {
+// public:
+//     int maxSubArray(vector<int>& nums) {
+//         int ans=nums[0],i,sum=0;
+//         for(i=0;i<nums.size();i++){
+//             sum+=nums[i];
+//             ans=max(sum,ans);
+//             sum=max(sum,0);
+//         }
+//         return ans;
+//     }
+// };

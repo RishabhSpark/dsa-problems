41090d4e72419636eed418c80b3f9da10dbbf7b1
RishabhSpark <rishabh1000khandelwal@gmail.com>
2023-02-02 20:21:52 +0530

    added

--- geeksforgeeks/linked_list.cpp
+++ geeksforgeeks/linked_list.cpp
@@ -12,10 +12,8 @@ struct Node{
 
 int main(){
     Node *head = new Node(10);
-    Node *temp1 = new Node(20);
-    Node *temp2 = new Node(30);
-    head -> next = temp1;
-    temp1 -> next = temp2;
+    head -> next = new Node(20);
+    head -> next -> next = new Node(30);
     while(head!=NULL){
         cout<<head->data<<" ";
         head = head -> next;

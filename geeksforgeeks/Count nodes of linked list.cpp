20c036014fa002d953e84b72d262b402dd333037
RishabhSpark <rishabh1000khandelwal@gmail.com>
2023-02-02 20:45:35 +0530

    added

--- geeksforgeeks/Count nodes of linked list.cpp
+++ geeksforgeeks/Count nodes of linked list.cpp
@@ -12,12 +12,10 @@ class Solution
     public:
     //Function to count nodes of a linked list.
     int getCount(struct Node* head){
-    
-        //Code here
         int count = 0;
         while(head!=NULL){
-            head=head->next;
             count++;
+            head = head->next;
         }
         return count;
     }

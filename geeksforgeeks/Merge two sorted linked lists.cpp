ed705feb40321f8c285994c14e98949d7ce20a63
RishabhSpark <rishabh1000khandelwal@gmail.com>
2023-02-05 16:33:06 +0530

    added

--- geeksforgeeks/Merge two sorted linked lists.cpp
+++ geeksforgeeks/Merge two sorted linked lists.cpp
@@ -13,25 +13,44 @@ struct Node {
 };
 */
 //Function to merge two sorted linked list.
+// Node* sortedMerge(Node* head1, Node* head2)  
+// {  
+//     // code here
+//     vector<int> arr1;
+//     while(head1!=NULL){
+//         arr1.push_back(head1->data);
+//         head1 = head1->next;
+//     }
+//     while(head2!=NULL){
+//         arr1.push_back(head2->data);
+//         head2 = head2->next;
+//     }
+//     sort(arr1.begin(), arr1.end());
+//     Node *merge = new Node(-1);
+//     Node *temp = merge;
+//     for(int i=0; i<arr1.size(); i++){
+//         merge->next = new Node(arr1[i]);
+//         merge = merge->next;
+//     }
+//     temp = temp->next;
+//     return temp;
+// }  
+
+
+// RECURSION
 Node* sortedMerge(Node* head1, Node* head2)  
 {  
     // code here
-    vector<int> arr1;
-    while(head1!=NULL){
-        arr1.push_back(head1->data);
-        head1 = head1->next;
+    Node *result = NULL;
+    if(head1==NULL) return head2;
+    if(head2==NULL) return head1;
+    if(head1->data >= head2->data){
+        result = head2;
+        result->next = sortedMerge(head1, head2->next);
     }
-    while(head2!=NULL){
-        arr1.push_back(head2->data);
-        head2 = head2->next;
+    else{
+        result = head1;
+        result->next = sortedMerge(head1->next, head2);
     }
-    sort(arr1.begin(), arr1.end());
-    Node *merge = new Node(-1);
-    Node *temp = merge;
-    for(int i=0; i<arr1.size(); i++){
-        merge->next = new Node(arr1[i]);
-        merge = merge->next;
-    }
-    temp = temp->next;
-    return temp;
+    return result;
 }  

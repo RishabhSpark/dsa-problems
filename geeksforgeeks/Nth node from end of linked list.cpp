8d5679dd1afc48e00f3c5a13c0480be2ad086f79
RishabhSpark <rishabh1000khandelwal@gmail.com>
2023-02-05 18:25:02 +0530

    added

--- geeksforgeeks/Nth node from end of linked list.cpp
+++ geeksforgeeks/Nth node from end of linked list.cpp
@@ -30,3 +30,23 @@ int getNthFromLast(Node *head, int n)
        }
        return rev->data;
 }
+
+
+// WITHOUT REVERSING THE LINKED LIST
+// int getNthFromLast(Node *head, int n)
+// {
+//        // Your code here
+//        Node *curr=head;
+//        int count=0;
+//        if(head==NULL) return 0;
+//        while(curr!=NULL){
+//            count++;
+//            curr = curr->next;
+//        }
+//        if(n>count) return -1;
+//        Node *curr1 = head;
+//        for(int i=1; i<count-n+1; i++){
+//            curr1 = curr1->next;
+//        }
+//        return curr1->data;
+// }

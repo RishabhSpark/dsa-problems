75fa6fe9299d6a54c7de9c0a541961c218161904
RishabhSpark <rishabh1000khandelwal@gmail.com>
2023-02-22 14:03:56 +0530

    added

--- leetcode/Algorithms/344. Reverse String.cpp
+++ leetcode/Algorithms/344. Reverse String.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        int n = s.size();
         for(int i=0; i<s.size()/2; i++){
             swap(s[i], s[s.size()-1-i]);
         }

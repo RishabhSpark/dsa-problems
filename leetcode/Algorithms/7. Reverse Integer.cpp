7f791f235167b11254d9b46f132da9f284f22bac
RishabhSpark <rishabh1000khandelwal@gmail.com>
2023-02-25 23:46:29 +0530

    added

---
+++ leetcode/Algorithms/7. Reverse Integer.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int reverse(int x) {
+        double rev = 0;
+        while(x!=0){
+            int digit = x%10;
+            rev+=digit;
+            rev*=10;
+            x/=10;
+        }
+        rev/=10;
+        if(rev > INT_MAX || rev < INT_MIN) return 0;
+        if(x<0){
+            return rev*=-1;
+        }
+        return rev;
+    }
+};

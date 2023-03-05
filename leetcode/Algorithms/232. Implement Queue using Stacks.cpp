56ad2e64d0aed598877c89f317566a24d5496011
RishabhSpark <rishabh1000khandelwal@gmail.com>
2023-03-06 00:06:47 +0530

    added

---
+++ leetcode/Algorithms/232. Implement Queue using Stacks.cpp
@@ -0,0 +1,41 @@
+class MyQueue {
+public:
+    stack<int> st;
+    MyQueue() {
+        
+    }
+    
+    void push(int x) {
+        if(st.empty()){
+            st.push(x);
+            return;
+        }
+        int data=st.top();
+        st.pop();
+        push(x);
+        st.push(data);
+    }
+    
+    int pop() {
+        int x=st.top();
+        st.pop();
+        return x;
+    }
+    
+    int peek() {
+        return st.top();
+    }
+    
+    bool empty() {
+        return st.empty();
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
